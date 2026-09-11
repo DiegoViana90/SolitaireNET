198bf99e8538298bd341a0e6982a4ea1b99c9e59
Diego.Viana <diego.viana@aspec.com.br>
2026-09-11 11:05:01 -0300

    fix(web): bust cached Firebase configuration

--- clients/web/auth.js
+++ clients/web/auth.js
@@ -1,4 +1,4 @@
-import { firebaseConfig, isFirebaseConfigured } from "./firebase-config.js";
+import { firebaseConfig, isFirebaseConfigured } from "./firebase-config.js?v=2";
 
 let auth = null;
 let firebaseModules = null;
